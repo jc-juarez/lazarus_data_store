1be9f141110d63e2f754d09194876350a02b1a5d
Juan Carlos Juárez <43016726+jc-juarez@users.noreply.github.com>
2025-11-14 22:43:04 -0800

    Add arm64 architecture (#2)

--- scripts/pre-build.sh
+++ scripts/pre-build.sh
@@ -10,14 +10,23 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-BUILD_IMAGE_NAME="build-ubuntu-lazarus"
+BUILD_IMAGE_NAME_AMD64="build-ubuntu-lazarus-amd64"
+BUILD_IMAGE_NAME_ARM64="build-ubuntu-lazarus-arm64"
 BUILD_DOCKERFILE="build.dockerfile"
 
-echo "[PRE-BUILD] Checking for Docker build image: ${BUILD_IMAGE_NAME}"
-
-if ! docker image inspect "$BUILD_IMAGE_NAME" >/dev/null 2>&1; then
+echo "[PRE-BUILD] Checking for Docker build image: ${BUILD_IMAGE_NAME_AMD64}"
+if ! docker image inspect "$BUILD_IMAGE_NAME_AMD64" >/dev/null 2>&1; then
     echo "[PRE-BUILD] Image not found — building it now..."
-    docker build --no-cache -t "$BUILD_IMAGE_NAME" -f "$BUILD_DOCKERFILE" .
+    docker buildx build --platform linux/amd64 --no-cache -t "$BUILD_IMAGE_NAME_AMD64" -f "$BUILD_DOCKERFILE" .
+    echo "[PRE-BUILD] Build image created successfully."
+else
+    echo "[PRE-BUILD] Build image already exists."
+fi
+
+echo "[PRE-BUILD] Checking for Docker build image: ${BUILD_IMAGE_NAME_ARM64}"
+if ! docker image inspect "$BUILD_IMAGE_NAME_ARM64" >/dev/null 2>&1; then
+    echo "[PRE-BUILD] Image not found — building it now..."
+    docker buildx build --platform linux/arm64 --no-cache -t "$BUILD_IMAGE_NAME_ARM64" -f "$BUILD_DOCKERFILE" .
     echo "[PRE-BUILD] Build image created successfully."
 else
     echo "[PRE-BUILD] Build image already exists."
